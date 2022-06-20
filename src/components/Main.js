d674e18b3230b6e353fb98c2ef5b01d6c4af701e
ayushi <choudharyayushi2812@gmail.com>
2022-06-20 22:15:20 +0530

    updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,15 +6,19 @@ function Main({props}) {
     <div>
        
       <h1>
-        New Car 
+        New Cars 
       </h1>
         {
           props.map((parameter) => {
             return (
               <>
-              <div className='main_div'>
-              <img src={parameter.profile_image_url} alt="image" />
+              <div className='main_div' >
+              <img src={parameter.profile_image_url} alt="im" />
               <h3>{parameter.v_model_display_name}</h3>
+              <h3>{parameter.v_model_min_price}-{parameter.v_model_max_price}</h3>
+              <h2>
+                View June Offers
+              </h2>
               </div>
               </>
             )
